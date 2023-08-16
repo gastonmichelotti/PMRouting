1be8b055d309417a76943b002622d3446de2f792
gastonmichelotti <gastonmichelotti@gmail.com>
2023-08-16 09:26:55 -0300

    wfastcgi

--- start_routingApi.bat
+++ start_routingApi.bat
@@ -6,6 +6,8 @@ call C:\Users\alejo\OneDrive\Escritorio\PMRouting\PMRoutingEnv\Scripts\activate
 REM Instala las dependencias
 pip install -r C:\Users\alejo\OneDrive\Escritorio\PMRouting\requirements.txt
 
+wfastcgi-enable
+
 REM Ejecuta tu API
 python C:\Users\alejo\OneDrive\Escritorio\PMRouting\run.py
 

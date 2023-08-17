921a10c51b8d2b6f922439d73ece140d2a8756f6
gastonmichelotti <gastonmichelotti@gmail.com>
2023-08-17 16:41:48 -0300

    Rutas  relativas

--- start_routingApi.bat
+++ start_routingApi.bat
@@ -1,14 +1,14 @@
 @echo off
 
 REM Activa el entorno virtual usando la ruta absoluta
-call C:\Users\alejo\OneDrive\Escritorio\PMRouting\PMRoutingEnv\Scripts\activate
+call .\PMRouting\PMRoutingEnv\Scripts\activate
 
 REM Instala las dependencias
-pip install -r C:\Users\alejo\OneDrive\Escritorio\PMRouting\requirements.txt
+pip install -r .\PMRouting\requirements.txt
 
 wfastcgi-enable
 
 REM Ejecuta tu API
-python C:\Users\alejo\OneDrive\Escritorio\PMRouting\run.py
+python .\PMRouting\run.py
 
 pause
